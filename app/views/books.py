6cb8a9b9f323b05db8efd6170263c45f5cdf84ae
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-05 20:26:07 -0400

    Refactor books view to implement pagination and category filtering

--- app/views/books.py
+++ app/views/books.py
@@ -1,27 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-
-
-mock_data = {
-    "user": {"name": "Admin Librarian", "email": "admin@library.com"},
-    "books": {"total_books": 1248, "added_month": 12},
-    "loans": {
-        "active_loans": 145,
-        "active_from_last_week": 22,
-        "overdue_loans": 12,
-        "overdue_from_last_week": 4,
-    },
-    "members": {"total_members": 573, "new_members": 18},
-    "new_members_last_seven_months": {
-        "Jan": 12,
-        "Feb": 15,
-        "Mar": 32,
-        "Apr": 18,
-        "May": 14,
-        "Jun": 11,
-        "Jul": 9,
-    },
-}
+from ..models import Book, Category
 
 
 def books(request):
-    return render(request, "pages/books.html", context=mock_data)
+    category = request.GET.get("category")
+    categories = Category.objects.values_list("name", flat=True)
+
+    books = Book.objects.all()
+
+    if category in categories:
+        books = books.filter(category__name=category)
+
+    paginator = Paginator(books, 5)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "pages/books.html", context={"books": page_obj})
