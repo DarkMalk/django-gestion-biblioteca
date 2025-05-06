08b90a13d950422e1a7f7c404e1a74eb28b1f30e
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-05 20:27:22 -0400

    Add URL patterns for loans, login, register, and logout views

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index, books
+from .views import index, books, loans, login, register, logout
 
 urlpatterns = [
     path("dashboard", index, name="dashboard"),
     path("books", books, name="books"),
+    path("", books, name="index"),
+    path("loans", loans, name="loans"),
+    path("login", login, name="login"),
+    path("register", register, name="register"),
+    path("logout", logout, name="logout"),
 ]
