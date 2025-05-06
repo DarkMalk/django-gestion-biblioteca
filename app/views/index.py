080c4ade7ceb44720d567b1e94a2d3dd9d8b78ea
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-05 20:27:07 -0400

    Refactor index view to remove mock data and use dynamic book data

--- app/views/index.py
+++ app/views/index.py
@@ -1,26 +1,46 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
-mock_data = {
-    "user": {"name": "Admin Librarian", "email": "admin@library.com"},
-    "books": {"total_books": 1248, "added_month": 12},
-    "loans": {
-        "active_loans": 145,
-        "active_from_last_week": 22,
-        "overdue_loans": 12,
-        "overdue_from_last_week": 4,
-    },
-    "members": {"total_members": 573, "new_members": 18},
-    "new_members_last_seven_months": {
-        "Jan": 12,
-        "Feb": 15,
-        "Mar": 32,
-        "Apr": 18,
-        "May": 14,
-        "Jun": 11,
-        "Jul": 9,
-    },
-}
+from ..models import Book
 
 
+@login_required
 def index(request):
-    return render(request, "pages/index.html", context=mock_data)
+    books = Book.objects.all()
+
+    total_books = books.count()
+    books_by_category = {}
+
+    for book in books:
+        category = book.category.name
+        if category in books_by_category:
+            books_by_category[category] += 1
+        else:
+            books_by_category[category] = 1
+
+    data = {
+        "total_books": total_books,
+        "books_by_category": books_by_category,
+        "total_members": 573,
+        "active_loans": 145,
+        "overdue_loans": 12,
+        "new_members_last_seven_months": {
+            "Jan": 12,
+            "Feb": 15,
+            "Mar": 32,
+            "Apr": 18,
+            "May": 14,
+            "Jun": 11,
+            "Jul": 9,
+        },
+        "library_activity_last_seven_months": {
+            "Jan": {"loans": 65, "returned": 42},
+            "Feb": {"loans": 59, "returned": 55},
+            "Mar": {"loans": 80, "returned": 70},
+            "Apr": {"loans": 81, "returned": 60},
+            "May": {"loans": 56, "returned": 45},
+            "Jun": {"loans": 55, "returned": 58},
+            "Jul": {"loans": 40, "returned": 45},
+        },
+    }
+
+    return render(request, "pages/index.html", context=data)
