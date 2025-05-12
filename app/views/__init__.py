4b1a3a4eb3863953de98bcccd331a6c5d296c5a0
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-12 02:36:17 -0400

    Refactor authentication views by consolidating login, logout, and register into auth module

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,7 +1,5 @@
 from .index import index
 from .books import books
 from .loans import loans
-from .login import login
-from .register import register
-from .logout import logout
+from .auth import login, register, logout
 from .members import members
