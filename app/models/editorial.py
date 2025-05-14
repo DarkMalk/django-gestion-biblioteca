c5cc1fe05ec68eaa14b9410585efcedbbaf83fe6
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-14 09:57:22 -0400

    Add unique constraint to name field in Editorial model and include created_at in EditorialAdmin list_display

--- app/models/editorial.py
+++ app/models/editorial.py
@@ -3,16 +3,17 @@ from django.contrib import admin
 
 
 class Editorial(models.Model):
-    name = models.CharField(max_length=80)
-    address = models.CharField(max_length=150, null=True)
-    email = models.EmailField(max_length=255, null=True)
+    name = models.CharField(max_length=80, unique=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class EditorialAdmin(admin.ModelAdmin):
-    list_display = ("name", "address", "email")
+    list_display = ("name", "address", "email", "created_at")
     search_fields = ("name",)
     ordering = ("id",)
     list_per_page = 10
