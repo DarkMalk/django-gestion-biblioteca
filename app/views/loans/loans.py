480d808ec41980785e20ce4307675d0381db7f8a
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-15 14:55:45 -0400

    Add loans management view and template with search and status filtering

--- app/views/loans/loans.py
+++ app/views/loans/loans.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from ..models import Loan
+from ...models import Loan
 
 
 @login_required
