80114e41a3120cd0987c397f18fce5b3a823f159
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-15 14:56:09 -0400

    Refactor imports in views to include new_book and new_loan functions

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,5 +1,5 @@
 from .index import index
-from .books import books
-from .loans import loans
+from .books import new_book, books
+from .loans import loans, new_loan
 from .auth import login, register, logout
 from .members import members
