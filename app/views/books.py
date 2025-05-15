c353d7fb5f1e6c2b730a508361c21e50638eb1ad
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-14 20:45:16 -0400

    Enhance books view to include search functionality and pass categories to template

--- app/views/books.py
+++ app/views/books.py
@@ -4,6 +4,7 @@ from ..models import Book, Category
 
 
 def books(request):
+    search = request.GET.get("search")
     category = request.GET.get("category")
     categories = Category.objects.values_list("name", flat=True)
 
@@ -12,9 +13,16 @@ def books(request):
     if category in categories:
         books = books.filter(category__name=category)
 
+    if search:
+        books = books.filter(title__icontains=search)
+
     paginator = Paginator(books, 5)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, "pages/books.html", context={"books": page_obj})
+    return render(
+        request,
+        "pages/books.html",
+        context={"books": page_obj, "categories": categories},
+    )
