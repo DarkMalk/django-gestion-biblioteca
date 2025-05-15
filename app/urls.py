e743165c0e59ecaed7a73488aec03abbe75cb07c
darkmalk <sa.vergaramunoz@gmail.com>
2025-05-15 14:56:23 -0400

    Add new routes for creating books and loans

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
-from .views import index, books, loans, login, register, logout, members
+from .views import (
+    index,
+    books,
+    loans,
+    login,
+    register,
+    logout,
+    members,
+    new_book,
+    new_loan,
+)
 
 urlpatterns = [
     path("dashboard", index, name="dashboard"),
     path("books", books, name="books"),
+    path("books/new", new_book, name="new_book"),
     path("", books, name="index"),
     path("loans", loans, name="loans"),
+    path("loans/new", new_loan, name="new_loan"),
     path("login", login, name="login"),
     path("register", register, name="register"),
     path("logout", logout, name="logout"),
