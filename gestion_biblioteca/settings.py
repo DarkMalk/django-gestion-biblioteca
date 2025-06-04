ab3d3f06ae13d3ac6d475639d25303a528e2804d
darkmalk <sa.vergaramunoz@gmail.com>
2025-06-04 15:43:24 -0400

    Refactor settings to use environment variables for SECRET_KEY and DEBUG, and update ALLOWED_HOSTS and STATIC_ROOT configurations

--- gestion_biblioteca/settings.py
+++ gestion_biblioteca/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pc3%e4vhwre95c=e2zcz7-7%!(hcpuj*l-br!gy+i%ps9xbygf"
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
